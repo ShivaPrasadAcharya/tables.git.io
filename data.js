d64422b14105501d3eb55c99a4cee4ace9748e0b
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2024-11-28 15:46:13 +0545

    Update data.js

--- data.js
+++ data.js
@@ -34,7 +34,7 @@ const tableData = [
     caption: "SKILL-WILL Matrix (सीप-इच्छा सम्बन्ध)",
     table: [
         ["--", "Low Skill", "High Skill"],
-        ["<b>High Will </b>", "<u>सिकारू कर्मचारी</u><br>•Incompetent but committed <br>•enthusiastic <br>•revolutionary<br>• के गर्ने, कहिले गर्ने, कसरी गर्ने भन्ने जान्दैन तर गर्न चाहन्छ<br> •needs empowerment", "<u>उत्कृष्ट कर्मचारी </u><br>• peak performer <br> role model,<br>• high achiever <br>•visionary<br>•गर्न जानेको छ र गरेको पनि छ<br> •needs recognition"],
+        ["<b>High Will </b>", "<u>सिकारू कर्मचारी</u><br>•Incompetent but committed <br>•enthusiastic <br>•revolutionary<br>• के गर्ने, कहिले गर्ने, कसरी गर्ने भन्ने जान्दैन तर गर्न चाहन्छ<br> •needs empowerment", "<u>उत्कृष्ट कर्मचारी </u><br>• peak performer <br> • role model,<br>• high achiever <br>•visionary<br>•गर्न जानेको छ र गरेको पनि छ<br> •needs recognition"],
         ["<b>Low Will</b>", "<u>निष्क्रिय कर्मचारी </u><br> • parasitic, <br>• doubtful, <br> shelter seeking <br>•गर्न पनि आउँदैन र गर्न पनि चाँहदैन <br>• needs intervention", "<u> निराश कर्मचारी </u><br> •capable but unmotivated <br>•compentent but discouraged <br>•गर्न आउँछ तर गर्न चाहँदैन<br>•need motivation"]
     ],
     footnote: "compiled by: Shiva Prasad Acharya, Supreme Court",
